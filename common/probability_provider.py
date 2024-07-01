cd2bb81365ab63310976887dcae5ee4396a03096
adamskrodzki@gmail.com <adamskrodzki@gmail.com>
2024-07-01 10:54:17 +0000

    update make_harder

--- common/probability_provider.py
+++ common/probability_provider.py
@@ -1,10 +1,11 @@
 import random
+
 class ProbabilityProvider:
     def __init__(self, decrement_percentage, num_sets):
-        self.decrement_percentage = decrement_percentage / 100.0  # Convert percentage to a decimal
-        self.probabilities = [1.0]  # Start with a probability of 1 for the first set
-        for i in range(1, num_sets):
-            self.probabilities.append(self.probabilities[-1] * (1 - self.decrement_percentage))
+        self.decrement_percentage = decrement_percentage / 100.0
+        self.probabilities = [1.0] + [
+            1.0 * (1 - self.decrement_percentage) ** i for i in range(1, num_sets)
+        ]
 
     def set_probabilities(self, new_probabilities):
         if len(new_probabilities) != len(self.probabilities):
@@ -15,14 +16,23 @@ class ProbabilityProvider:
         return self.probabilities
 
     def make_harder(self):
-        non_zero_index = next((i for i, prob in enumerate(reversed(self.probabilities)) if prob != 0), None)
-        if non_zero_index is not None:
-            if non_zero_index > 0:
-                self.probabilities[non_zero_index-1] = 0.5
-            
-        if non_zero_index > 1:
-            fraction = 0.5/(non_zero_index - 1)
-            self.probabilities[:non_zero_index-1]=[fraction] * (non_zero_index - 1)
+        first_zero_index = next((i for i, prob in enumerate(self.probabilities) if prob == 0), None)
+        if first_zero_index is None:
+            raise ValueError("No zero element found in probabilities.")
+
+        self.probabilities[first_zero_index] = 1.0
+
+        if first_zero_index > 0:
+            self.probabilities[first_zero_index - 1] = 0.5
+
+        if first_zero_index > 1:
+            fraction = 0.5 / (first_zero_index - 1)
+            self.probabilities = [
+                fraction if i < first_zero_index - 1 else prob 
+                for i, prob in enumerate(self.probabilities)
+            ]
+
+        print(self.probabilities)
 
     def sample(self):
         if all(prob == 0 for prob in self.probabilities):
